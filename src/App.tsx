d3140fe8fd9bc833c053185022f5fea0b6862a27
anzor1qqqqq <anzor.shadzhe@gmail.com>
2025-11-20 17:21:20 +0300

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 
 export function App() {
   const [content, setContent] = useState('')
+  console.log('content: ', content);
 
   useEffect(() => {
-    const pages = import.meta.glob('/public/file.txt', { eager: true, query: 'raw' });
+    const pages: any = import.meta.glob('/public/file.txt', { eager: true, query: 'raw' });
 
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setContent(pages['/public/file.txt'].default)
   }, [])
 
